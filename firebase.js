94a90a5194ab32a3f604d7feb98669fed0075c2d
Smjkrh <eboy3502@gmail.com>
2026-02-11 23:02:30 +0900

    initial clean commit

--- firebase.js
+++ firebase.js
@@ -1,10 +1,9 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
+const admin = require("firebase-admin");
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
-const db = admin.firestore();
-
-module.exports = db;
+module.exports = admin;
