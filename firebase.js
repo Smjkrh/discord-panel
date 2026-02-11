f9ea2cd20aecb214086dc52e836d69434ce4c9d2
Smjkrh <eboy3502@gmail.com>
2026-02-11 23:11:28 +0900

    fix firebase

--- firebase.js
+++ firebase.js
@@ -3,7 +3,9 @@ const admin = require("firebase-admin");
 const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
 });
 
-module.exports = admin;
+const db = admin.firestore();
+
+module.exports = db;
